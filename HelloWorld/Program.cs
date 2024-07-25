952fb1b1eb5d90a97d51e5387ba48237ab872236
Foxglovery <foxglovery@gmail.com>
2024-07-25 11:57:24 -0500

    excercises

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,25 +11,58 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string textAge = "-23";
-            int age = Convert.ToInt32(textAge);
-            Console.WriteLine(age);
+            //string textAge = "-23";
+            //int age = Convert.ToInt32(textAge);
+            //Console.WriteLine(age);
 
-            string textBigNumber = "900000000";
-            long bigNumber = Convert.ToInt64(textBigNumber);
-            Console.WriteLine(bigNumber);
+            // using var lets the compiler decide the data type
+            // some types still need the appendage (L,F,D) Dont do this too much
+            // var age = 23;
+            // Console.WriteLine(age);
 
-            string textNegative = "-55.2";
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine(negative);
+            // Constants
 
-            string textPrecision = "5.00001";
-            float precision = Convert.ToSingle(textPrecision);
-            Console.WriteLine(precision);
+            //const int vat = 20;
+            //const double percentVAT = vat / 100D;
+            //int balance = 1000;
 
-            string textMoney = "14.99";
-            decimal money = Convert.ToDecimal(textMoney);
-            Console.WriteLine(money);
+            //Console.WriteLine(balance * percentVAT);
+
+            // converting types
+            //string textBigNumber = "900000000";
+
+            //long bigNumber = Convert.ToInt64(textBigNumber);
+            //Console.WriteLine(bigNumber);
+
+            //string textNegative = "-55.2";
+            //double negative = Convert.ToDouble(textNegative);
+            //Console.WriteLine(negative);
+
+            //string textPrecision = "5.00001";
+            //float precision = Convert.ToSingle(textPrecision);
+            //Console.WriteLine(precision);
+
+            //string textMoney = "14.99";
+            //decimal money = Convert.ToDecimal(textMoney);
+            //Console.WriteLine(money);
+
+            //string name = "Gabriel";
+            //Console.WriteLine(name);
+
+            //string phoneNum = "9321877733";
+            //Console.WriteLine(phoneNum);
+
+            //int age = 35;
+            //Console.WriteLine(age);
+
+            // REMAINDER 
+            int firstNum = 1675;
+            int secondNum = 12;
+            int remainder = firstNum % secondNum;
+            Console.WriteLine(remainder);
+
+
+            
 
             Console.ReadLine();
         }
